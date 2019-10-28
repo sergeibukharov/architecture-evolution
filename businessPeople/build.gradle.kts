00f652bd846fa1c804c7967d0d573c79fa2d71f7
Sergey <sergey.bukharov@thoughtworks.com>
2019-10-28 19:30:36 +0800

    business layer ks tested with junit and gradle

--- businessPeople/build.gradle.kts
+++ businessPeople/build.gradle.kts
@@ -9,4 +9,9 @@ dependencies {
 
     // tests
     testCompile("org.junit.jupiter:junit-jupiter-api:5.5.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
