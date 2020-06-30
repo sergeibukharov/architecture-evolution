c93e93c8401c1808f95c58c9a8dac557e8109fc0
Sergey Bukharov <bukharovSI@gmail.com>
2020-06-30 22:29:24 +0800

    fix if quote garden does not work

--- src/main/kotlin/com/thoughtworks/people/utils/GeneratedQuote.kt
+++ src/main/kotlin/com/thoughtworks/people/utils/GeneratedQuote.kt
@@ -1,5 +1,6 @@
 package com.thoughtworks.people.utils
 
+import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
 
 const val defaultQuote = "whoops, something went wrong"
@@ -8,10 +9,18 @@ class GeneratedQuote {
 
     private val getRandomUrl = "https://quote-garden.herokuapp.com/quotes/random"
 
+    private val httpRequestFactory = SimpleClientHttpRequestFactory()
+            .also { it.setConnectTimeout(5000) }
+            .also { it.setReadTimeout(5000) }
 
-    fun get() = RestTemplate()
-            .getForEntity(getRandomUrl, QuoteResponse::class.java)
-            .body?.quoteText ?: defaultQuote
+    fun get() =
+            try {
+                RestTemplate(httpRequestFactory)
+                        .getForEntity(getRandomUrl, QuoteResponse::class.java)
+                        .body?.quoteText ?: defaultQuote
+            } catch (e: Exception) {
+                defaultQuote
+            }
 
     internal data class QuoteResponse(
             val _id: String,
